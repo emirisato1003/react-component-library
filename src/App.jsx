a420d20412add220395edb677cf04a2cb5f612ce
emirisato1003 <emirisato.1003@gmail.com>
2025-02-22 13:54:07 -0800

    complete square badges component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Badges from './assets/Badges';
 // import Menu from './Menu/Menu';
 
 function App() {
+  const badges = ['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
   return (
     // <Star onChange={() => { }} />
     <main>
-    <h1>React Component Library</h1>
-    <h2>Badges</h2>
-    <h3>Square</h3>
-    <Badges color={'gray'}>Badges</Badges>
-    <h3>Pill</h3>
+      <h1>React Component Library</h1>
+      <h2>Badges</h2>
+      <h3>Square</h3>
+      <div className="square-badges">
+        {
+          badges.map(color =>
+            <Badges color={color}>Badges</Badges>)
+        }
+      </div>
+
+      <h3>Pill</h3>
     </main>
   );
 }
