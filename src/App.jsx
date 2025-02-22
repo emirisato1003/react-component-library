51ac01df091bb5a97898a1b78fecaa669945df20
emirisato1003 <emirisato.1003@gmail.com>
2025-02-22 13:25:29 -0800

    update font family

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Star from './assets/Star';
 function App() {
   return (
     // <Star onChange={() => { }} />
+    <main>
     <h1>React Component Library</h1>
+    <h2>Badges</h2>
+    <h3>Square</h3>
+    <h3>Pill</h3>
+    </main>
   );
 }
 
