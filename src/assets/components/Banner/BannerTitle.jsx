52aa4225cd78114e289960c9b68c6dd4eedf31f2
emirisato1003 <emirisato.1003@gmail.com>
2025-02-24 11:20:31 -0800

    set button component in bannerTitle.jsx

--- src/assets/components/Banner/BannerTitle.jsx
+++ src/assets/components/Banner/BannerTitle.jsx
@@ -1,7 +1,10 @@
-
+import { useContext } from "react";
+import Button from "../Button/Button";
+import { BannerContext } from "./Banner";
 
 export default function BannerTitle({ children }) {
+    const { toggleOpen } = useContext(BannerContext)
     return (
-        <h5>{children}</h5>
+        <Button>{children}</Button>
     );
 }
