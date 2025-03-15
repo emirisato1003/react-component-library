b7552d07e5b2939c01c3a63d21c93734bcfc50b1
Emiri Sato <emirisato.1003@gmail.com>
2025-03-15 15:44:04 -0700

    update portal.jsx

--- src/assets/components/ToolTips/Portal.jsx
+++ src/assets/components/ToolTips/Portal.jsx
@@ -3,7 +3,6 @@ import { createPortal } from 'react-dom';
 export default function Portal({title, text, icon}) {
     return (
         <div style={{ border: '2px solid black' }}>
-            <p>children</p>
             {createPortal(
                 <p>This child is placed in the document body.</p>,
                 document.body
