ceeb9373e99865588b035fd9e7f8726a578fefdd
Emiri Sato <emirisato.1003@gmail.com>
2025-03-31 16:39:01 -0700

    fix image url

--- src/assets/components/Testimonials/Testimonial.jsx
+++ src/assets/components/Testimonials/Testimonial.jsx
@@ -1,17 +1,17 @@
 import classNames from "classnames";
 import { createContext } from "react";
 
-const TestimonialContext = createContext()
+const TestimonialContext = createContext();
 
 export default function Testimonial({ children, imgUrl }) {
     const testimonialClassName = classNames(imgUrl ? 'testimonial-container' : 'testimonial-withNoImg-container');
     return (
-        <TestimonialContext.Provider value={{imgUrl}}>
+        <TestimonialContext.Provider value={{ imgUrl }}>
             <div className={testimonialClassName}>
                 {imgUrl ? <img src={imgUrl} alt="testimonial person portrait" /> :
                     <>
-                        <img src="src/assets/logo/Logo.png" />
-                        <img className='dot_pattern' src="src/assets/logo/dot_pattern.png" alt="" />
+                        <img src="src\assets\logo\Logo.png" />
+                        <img className='dot_pattern' src="src\assets\logo\dot_pattern.png" alt="" />
                     </>
                 }
                 {children}
@@ -20,4 +20,4 @@ export default function Testimonial({ children, imgUrl }) {
     );
 }
 
-export {TestimonialContext}
+export { TestimonialContext };
